a88e221a0202d0e1a5f5b171b6f5fa8e53c00c22
vvaks <vvaks@hortonworks.com>
2016-11-09 19:55:55 -0500

    script to stop data simulation

--- stopSimulation.sh
+++ stopSimulation.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-kill -9 `cat RetailStoreSim.pid`
-kill -9 `cat SocialMediaSim.pid`
+kill -9 `cat STB_1000_Sim.pid`
+kill -9 `cat STB_2000_Sim.pid`
+kill -9 `cat STB_3000_Sim.pid`
+
+kill -9 `cat Technician_1000_Sim.pid`
+kill -9 `cat Technician_2000_Sim.pid`
+kill -9 `cat Technician_3000_Sim.pid`
