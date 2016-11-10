0826898f0eb30f4d7d98a1fb90f439539202a7e0
vvaks <vvaks@hortonworks.com>
2016-11-10 16:21:06 -0500

    update

--- startDemoServices.sh
+++ startDemoServices.sh
@@ -360,7 +360,7 @@ startNifiFlowReporter
 
 echo "*********************************Deploying Spark Streaming Application..."
 #nohup spark-submit --class com.hortonworks.iot.spark.streaming.SparkNostradamus --master local[4] /home/spark/DeviceMonitorNostradamus-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 $ROOT_PATH/DeviceMonitorNostradamus.log &
-nohup spark-submit --class com.hortonworks.iot.spark.streaming.SparkNostradamus --master yarn /home/spark/DeviceMonitorNostradamus-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 $ROOT_PATH/DeviceMonitorNostradamus.log &
+nohup spark-submit --class com.hortonworks.iot.spark.streaming.SparkNostradamus --master yarn --executor-memory 1G --num-executors 2 /home/spark/DeviceMonitorNostradamus-0.0.1-SNAPSHOT-jar-with-dependencies.jar 2>&1 $ROOT_PATH/DeviceMonitorNostradamus.log &
 
 echo "*********************************Deploying Application Container to..."
 # Ensure docker service is running
