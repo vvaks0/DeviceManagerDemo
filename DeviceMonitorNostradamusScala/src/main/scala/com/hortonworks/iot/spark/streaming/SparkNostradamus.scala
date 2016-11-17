628804c4b8d910d12f3e369f1fdf5420c85564d3
vvaks <vvaks@hortonworks.com>
2016-11-16 22:51:12 -0500

    update

--- DeviceMonitorNostradamusScala/src/main/scala/com/hortonworks/iot/spark/streaming/SparkNostradamus.scala
+++ DeviceMonitorNostradamusScala/src/main/scala/com/hortonworks/iot/spark/streaming/SparkNostradamus.scala
@@ -5,18 +5,25 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka._
 import org.apache.spark.mllib.classification.SVMModel
 import org.apache.spark.mllib.linalg.Vectors
+import org.cometd.client.BayeuxClient
+import org.cometd.client.transport.{ClientTransport,LongPollingTransport}
+import org.eclipse.jetty.client.HttpClient
 import scala.util.parsing.json._
 
 object SparkNostradamus {
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("SparkNostradamus")
-    // Create the context
     val ssc = new StreamingContext(sparkConf, Seconds(1))
     ssc.sparkContext.setLogLevel("WARN")
     ssc.checkpoint("/demo/data/checkpoint");
     val nostradamus = SVMModel.load(ssc.sparkContext, "/demo/data/model/nostradamusSVMModel");
-    val kafkaTopicConfig = Map(args(1) -> 1)    
-    val deviceStreamJSON = KafkaUtils.createStream(ssc, args(0), "spark-streaming-group", kafkaTopicConfig)
+    val kafkaConsumerGroup = "spark-streaming-group"
+    val zkQuorum = args(0)
+    val deviceEventsTopic = args(1)
+    val cometdUrl = ssc.sparkContext.broadcast("http://" + args(2) + "/cometd")
+    val predictionChannel = ssc.sparkContext.broadcast("/prediction")
+    val kafkaTopicConfig = Map(deviceEventsTopic -> 1)    
+    val deviceStreamJSON = KafkaUtils.createStream(ssc, zkQuorum, kafkaConsumerGroup, kafkaTopicConfig)
     val deviceStream = deviceStreamJSON.map{ rdd => val deviceStatusEvent = JSON.parseFull(rdd._2).getOrElse("{}").asInstanceOf[Map[String,Any]]
                                                     val serialNumber = deviceStatusEvent.get("serialNumber").get.asInstanceOf[String]
                                                     val internalTemp = deviceStatusEvent.get("internalTemp").get.asInstanceOf[Int].toDouble
@@ -24,6 +31,7 @@ object SparkNostradamus {
                                             }.updateStateByKey(fillFeatureList).foreachRDD(rdd => {
                                                                                 rdd.foreach(featureTupleList =>
                                                                                 {
+                                                                                  val tempFailPredication = "Temperature pattern indicates imminent device failure. Contact customer or send technician"
                                                                                   val serialNumber = featureTupleList._1
                                                                                   val featureList = featureTupleList._2
                                                                                   if(featureList.size == 10){
@@ -35,6 +43,19 @@ object SparkNostradamus {
 								                                                                      println("*********************************************************************************")
 								                                                                      println("**********************DEVICE FAILURE IMMINENT: " + serialNumber + " : " + prediction + " : " + predictionFeatures)
 								                                                                      println("*********************************************************************************")
+								                                                                      val httpClient = new HttpClient
+								                                                                      httpClient.start
+                                                                                      val bayuexClient = new BayeuxClient(cometdUrl.value, new LongPollingTransport( new java.util.HashMap[String,Object](), httpClient))
+								                                                                      bayuexClient.handshake
+								                                                                      if (bayuexClient.waitFor(3000, BayeuxClient.State.CONNECTED)){
+		                                                                                    println("Connected to Cometd Http PubSub Platform")
+								                                                                        val data = new java.util.HashMap[String,String]()
+								                                                                        data.put("deviceSerialNumber", serialNumber)
+								                                                                        data.put("predictionDescription", tempFailPredication)
+								                                                                        bayuexClient.getChannel(predictionChannel.value).publish(data)
+								                                                                      }else{
+			                                                                                  println("Could not connect to Cometd Http PubSub Platform")
+								                                                                      }  
 							                                                                      }else{
 								                                                                      println("*********************************************************************************")
 								                                                                      println("**********************DEVICE FUNCTION NORMAL : " + serialNumber + " : " + prediction + " : " + predictionFeatures)
